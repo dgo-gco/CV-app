a8612e70394766e57026d63ccbcb2839478bf4cb
dgo <jdiegogarciaco@gmail.com>
2023-11-01 16:54:25 +0100

    style: add icons change input style

--- src/components/cv/CVSheet.jsx
+++ src/components/cv/CVSheet.jsx
@@ -1,40 +1,54 @@
-import { useCurriculumContext } from '../context/CurriculumContext';
-import '../../styles/CVsheet.css';
+import { useCurriculumContext } from "../context/CurriculumContext";
+import "../../styles/CVsheet.css";
+import { BsTelephone } from "react-icons/bs";
+import { AiOutlineMail } from "react-icons/ai";
 
 export const CVSheet = () => {
   const { personalData, schoolsInfo, companiesInfo } = useCurriculumContext();
 
   return (
-    <div className='main-sheet'>
-      <div className='main-sec'>
-        <h1 className='name'>{personalData.name && personalData.name}</h1>
-        <p>{personalData.name && personalData.email}</p>
-        <p>{personalData.name && personalData.phone}</p>
+    <div className="main-sheet">
+      <div className="main-sec">
+        <h1 className="name">{personalData.name && personalData.name}</h1>
+        <p className="icon-txt">
+          {personalData.email && (
+            <span>
+              <AiOutlineMail /> {personalData.email}
+            </span>
+          )}
+        </p>
+        <p className="icon-txt">
+          {personalData.phone && (
+            <span>
+              <BsTelephone /> {personalData.phone}
+            </span>
+          )}
+        </p>
       </div>
-      <div className='education-sec'>
+      <div className="education-sec">
         <h4>Education</h4>
         {schoolsInfo &&
           schoolsInfo.map((school) => (
-            <div key={school.id} className='sections'>
-              <div className='dates-sec'>
+            <div key={school.id} className="sections">
+              <div className="dates-sec">
                 {school.startDate} / {school.endDate}
               </div>
-              <div className='school-info'>
+              <div className="school-info">
                 <p>{school.school}</p>
                 <p>{school.degree}</p>
               </div>
             </div>
           ))}
       </div>
-      <div className='company-sec'>
+      <div className="company-sec">
         <h4>Professional</h4>
         {companiesInfo &&
           companiesInfo.map((company) => (
-            <div key={company.id} className='sections'>
-              <div className='dates-sec'>
+            <div key={company.id} className="sections">
+              <div className="dates-sec">
                 {company.startDate} / {company.endDate}
               </div>
-              <div className='company-info'>
+              <div className="company-info">
                 <p>{company.company}</p>
                 <p>{company.responsabilities}</p>
               </div>
